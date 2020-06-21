937535c02a269230737562d7200e00ca60ef18cd
boisseau <sboisseau@aepsilon.com>
2020-06-21 14:11:39 +0200

    Ajout notion de produit

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,8 +1,23 @@
 // Components/Search.tsx
 
 import React from 'react'
-import { View, TextInput, Button } from 'react-native'
+import { StyleSheet,View, TextInput, Button, FlatList, Text } from 'react-native'
+import {PRODUCTS} from '../helpers/produits-data'
 
+const styles =  StyleSheet.create({
+  main_container: {
+    flex: 1,
+    marginTop: 20
+  },
+  textinput: {
+    marginLeft: 5,
+    marginRight: 5,
+    height: 50,
+    borderColor: '#000000',
+    borderWidth: 1,
+    paddingLeft: 5
+  }
+})
 
 type SearchProps = {
   // using `interface` is also ok
@@ -15,9 +30,14 @@ type SearchState = {
 export default class Search extends React.Component<SearchProps, SearchState>  {
   render() {
     return (
-      <View>
-        <TextInput placeholder='Titre du film'/>
+      <View style={styles.main_container}>
+        <TextInput style={styles.textinput} placeholder='Titre du film'/>
         <Button title='Rechercher' onPress={() => {}}/>
+        <FlatList
+          data={PRODUCTS}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({item}) => <Text>{item.nom}</Text>}
+        />
       </View>
     )
   }
---
+++ helpers/produits-data.tsx
@@ -0,0 +1,18 @@
+import { Produit } from "../model/produit";
+
+
+export  const PRODUCTS : Produit[]  = [
+    {
+       id:181810,
+       nom:"JetLag",
+       prix:10.5
+    },
+    {
+       id:181809,
+       nom:"Pof",
+       prix:5.8
+    }
+ ] ;
+ 
+
+ 
